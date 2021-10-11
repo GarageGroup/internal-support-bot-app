3ce4b4ce053692efb62cc61ac0f35ac89fbe9e14
pmosk <pm@ggroupp.ru>
2021-10-11 20:26:33 +0300

    Add adaptive card for customer choosing

---
+++ src/Support.Bot.Application/Flows/Incident.Customer.Find/Internal.Card/CustomerChooseDataJson.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace GGroupp.Internal.Support.Bot;
+
+internal sealed record IncidentCreateActionDataJson
+{
+    public static readonly IncidentCreateActionDataJson Create = new()
+    {
+        ActionName = "Create"
+    };
+
+    public static readonly IncidentCreateActionDataJson Cancel = new()
+    {
+        ActionName = "Cancel"
+    };
+
+    [JsonProperty("actionName")]
+    [JsonPropertyName("actionName")]
+    public string? ActionName { get; init; }
+}
+
