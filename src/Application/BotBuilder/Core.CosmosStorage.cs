d3ec8cd6aa994b53af69a7d19af58898e4674e47
pmosk <pm@ggroupp.ru>
2022-06-24 14:02:29 +0300

    Add proactive messages support

--- src/Application/BotBuilder/Core.CosmosStorage.cs
+++ src/Application/BotBuilder/Core.CosmosStorage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading;
 using GGroupp.Infra.Bot.Builder;
-using Microsoft.Bot.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PrimeFuncPack;
@@ -11,31 +10,37 @@ namespace GGroupp.Internal.Support;
 
 partial class GSupportBotBuilder
 {
-    internal static IStorage ResolveCosmosStorage(IServiceProvider serviceProvider)
+    internal static ICosmosStorage ResolveCosmosStorage(IServiceProvider serviceProvider)
         =>
         lazyCosmosStorageDependency.Value.Resolve(serviceProvider);
 
-    private static readonly Lazy<Dependency<IStorage>> lazyCosmosStorageDependency
+    private static readonly Lazy<Dependency<ICosmosStorage>> lazyCosmosStorageDependency
         =
         new(CreateCosmosStorageDependency, LazyThreadSafetyMode.ExecutionAndPublication);
 
-    private static Dependency<IStorage> CreateCosmosStorageDependency()
+    private static Dependency<ICosmosStorage> CreateCosmosStorageDependency()
         =>
         CreateStandardHttpHandlerDependency("CosmosStorage")
+        .UseCosmosApi(
+            sp => sp.GetRequiredService<IConfiguration>().GetCosmosApiOption())
         .UseCosmosStorage(
-            sp => sp.GetRequiredService<IConfiguration>().GetCosmosStorageConfiguration());
+            sp => sp.GetRequiredService<IConfiguration>().GetCosmosStorageOption());
 
-    private static CosmosStorageConfiguration GetCosmosStorageConfiguration(this IConfiguration configuration)
+    private static CosmosApiOption GetCosmosApiOption(this IConfiguration configuration)
         =>
         new(
             baseAddress: new(configuration.GetValue<string>("CosmosDbBaseAddressUrl")),
             masterKey: configuration.GetValue<string>("CosmosDbMasterKey"),
-            databaseId: configuration.GetValue<string>("CosmosDbDatabaseId"),
-            containerTtlSeconds: new Dictionary<CosmosStorageContainerType, int?>
+            databaseId: configuration.GetValue<string>("CosmosDbDatabaseId"));
+
+    private static CosmosStorageOption GetCosmosStorageOption(this IConfiguration configuration)
+        =>
+        new(
+            containerTtlSeconds: new Dictionary<StorageItemType, int?>
             {
-                [CosmosStorageContainerType.UserState] = configuration.GetTtlSeconds("CosmosDbUserStateContainerTtlHours"),
-                [CosmosStorageContainerType.ConversationState] = configuration.GetTtlSeconds("CosmosDbConversationStateContainerTtlHours"),
-                [CosmosStorageContainerType.BotStorage] = configuration.GetTtlSeconds("CosmosDbBotStorageContainerTtlHours")
+                [StorageItemType.UserState] = configuration.GetTtlSeconds("CosmosDbUserStateContainerTtlHours"),
+                [StorageItemType.ConversationState] = configuration.GetTtlSeconds("CosmosDbConversationStateContainerTtlHours"),
+                [StorageItemType.Default] = configuration.GetTtlSeconds("CosmosDbBotStorageContainerTtlHours")
             });
 
     private static int? GetTtlSeconds(this IConfiguration configuration, string ttlHoursKey)
