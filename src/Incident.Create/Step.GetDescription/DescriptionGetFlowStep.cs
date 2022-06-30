7c4d778da809c6e1d9be769732b767d92c6521ba
pmosk <pm@ggroupp.ru>
2022-06-30 21:15:27 +0300

    Allow start with picture

--- src/Incident.Create/Step.GetDescription/DescriptionGetFlowStep.cs
+++ src/Incident.Create/Step.GetDescription/DescriptionGetFlowStep.cs
@@ -1,4 +1,6 @@
 using GGroupp.Infra.Bot.Builder;
+using Microsoft.Bot.Builder;
+using Newtonsoft.Json.Linq;
 
 namespace GGroupp.Internal.Support;
 
@@ -10,16 +12,43 @@ internal static class DescriptionGetFlowStep
 
     private static ChatFlowJump<IncidentCreateFlowState> GetDescriptionOrFailure(IChatFlowContext<IncidentCreateFlowState> context)
     {
-        var text = context.Activity.Text;
+        var description = context.GetDescription();
 
-        if (string.IsNullOrEmpty(text) || context.GetCardActionValueOrAbsent().IsPresent)
+        if (string.IsNullOrEmpty(description))
         {
             return default;
         }
 
         return context.FlowState with
         {
-            Description = context.Activity.Text
+            Description = description
         };
+    }
+
+    private static string? GetDescription(this ITurnContext turnContext)
+    {
+        var text = turnContext.Activity.Text;
+
+        if (turnContext.GetCardActionValueOrAbsent().IsPresent)
+        {
+            return default;
+        }
+
+        if (string.IsNullOrEmpty(text) is false)
+        {
+            return text;
+        }
+
+        if (turnContext.IsNotTelegramChannel())
+        {
+            return default;
+        }
+
+        if (turnContext.Activity.ChannelData is not JObject jObject)
+        {
+            return default;
+        }
+
+        return jObject.SelectToken("message.caption")?.ToString();
     }
 }
