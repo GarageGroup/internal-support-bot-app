1373f9f499cda0886e20f34b2beaa2c4d5c970d3
pmosk <pm@ggroupp.ru>
2022-07-05 15:18:27 +0300

    Use service bus

--- src/Application/BotBuilder/Command.IncidentCreate.cs
+++ src/Application/BotBuilder/Command.IncidentCreate.cs
@@ -36,18 +36,16 @@ partial class GSupportBotBuilder
     private static IIncidentCreateFlowSender GetIncidentCreateFlowSender(IBotContext botContext)
         =>
         Dependency.From(ResolveIncidentCreateQueueOption)
-        .UseQueueWriter<FlowMessage<IncidentCreateFlowMessage>>()
+        .UseServiceBusQueueWriter<FlowMessage<IncidentCreateFlowMessage>>()
         .Resolve(botContext.ServiceProvider);
 
-    private static QueueOption ResolveIncidentCreateQueueOption(IServiceProvider serviceProvider)
+    private static ServiceBusQueueOption ResolveIncidentCreateQueueOption(IServiceProvider serviceProvider)
         =>
         serviceProvider.GetRequiredSection("IncidentCreateQueue").GetQueueOption();
 
-    private static QueueOption GetQueueOption(this IConfigurationSection section)
+    private static ServiceBusQueueOption GetQueueOption(this IConfigurationSection section)
         =>
         new(
-            queueConnectionString: section["ConnectionString"],
-            queueName: section["Name"],
-            visibilityTimeout: section.GetValue<TimeSpan?>("VisibilityTimeout"),
-            timeToLive: section.GetValue<TimeSpan?>("TimeToLive"));
+            connectionString: section["ConnectionString"],
+            name: section["Name"]);
 }
